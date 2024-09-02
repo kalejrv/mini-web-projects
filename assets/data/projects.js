e5a3825bc16468dce028b6bed60bf84c4172ff2b
Kevin Reyes <kreyesvelasquez@gmail.com>
2024-09-02 09:51:11 -0600

    Add homepage files

---
+++ assets/data/projects.js
@@ -0,0 +1,26 @@
+const buildProjectData = projectName => {
+  const imagePath = "/assets/images";
+  const viewUrl = "/projects";
+  const codeUrl = "https://github.com/kalejrv/mini-web-projects/tree/main/projects";
+
+  return {
+    title: buildProjectTitle(projectName),
+    image: `${imagePath}/${projectName}.png`,
+    view: `${viewUrl}/${projectName}`,
+    code: `${codeUrl}/${projectName}`,
+  };
+};
+
+const buildProjectTitle = str => {
+  const strSplitted = str.split("-");
+  const strFirstWordCapitalized = strSplitted[0][0].toUpperCase() + strSplitted[0].slice(1);
+  const strJoined = `${strFirstWordCapitalized} ${strSplitted.slice(1).join(" ")}.`;
+
+  return strJoined;
+};
+
+const projects = [
+  // buildProjectData("expanding-cards"),
+];
+
+export default projects;
---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+import projects from "../data/projects.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  addProjectCards();
+});
+
+const addProjectCards = () => {
+  const projectCardsWrapper = document.querySelector(".project-cards-wrapper");
+
+  projects.map(project => {
+    const projectCard = createProjectCard(project);
+
+    projectCardsWrapper.appendChild(projectCard);
+  });
+};
+
+const createProjectCard = project => {
+  const { image, title, view, code } = project;
+
+  const projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+  projectCard.innerHTML = `
+    <img src="${image}" alt="${title} image." class="project-image">
+  
+    <div class="project-content">
+      <h2 class="project-title">${title}</h2>
+      
+      <div class="project-links-wrapper">  
+        <a href="${view}" target="_blank">
+          View
+          <img src="/assets/icons/view.svg">
+        </a>
+          
+        <a href="${code}" target="_blank">
+          Code
+          <img src="/assets/icons/code.svg">
+        </a>  
+      </div>
+    </div>
+  `;
+
+  return projectCard;
+};
